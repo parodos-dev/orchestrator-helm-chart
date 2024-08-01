2ba5fb8ff2df444de85edd45d853bce7297f6c75
Roy Golan <rgolan@redhat.com>
2024-08-01 16:47:18 +0300

    Helper nightly action to print the orchestrator plugin versions
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- charts/orchestrator-k8s/hack/update_plugins_version.sh
+++ charts/orchestrator-k8s/hack/update_plugins_version.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Get the directory of the script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -8,29 +8,24 @@ TEMPLATE_FILE="${SCRIPT_DIR}/../values.yaml"
 
 # Define PLUGINS list
 PLUGINS=(
-  "@janus-idp/plugin-notifications"
-  "@janus-idp/plugin-notifications-backend-dynamic"
+  # TODO notifications are not published upstream for now.
+  #"@janus-idp/plugin-notifications"
+  #"@janus-idp/plugin-notifications-backend-dynamic"
   "@janus-idp/backstage-plugin-orchestrator"
   "@janus-idp/backstage-plugin-orchestrator-backend-dynamic"
 )
 
-# Function to fetch plugin information
-get_plugin_info() {
-  PLUGIN_NAME=$1
-  curl -s -q "https://registry.npmjs.com/${PLUGIN_NAME}" | \
-  jq -r '.versions | keys_unsorted[-1] as $latest_version |
-  .[$latest_version] | "\(.version)#\(.dist.integrity)"'
-}
 
 # Function to update YAML file with plugin information
 update_plugins_in_yaml() {
   for PLUGIN_NAME in "${PLUGINS[@]}"; do
-    PLUGIN_VERSION=$(get_plugin_info "$PLUGIN_NAME" | cut -d '#' -f 1)
-    PLUGIN_INTEGRITY=$(get_plugin_info "$PLUGIN_NAME" | cut -d '#' -f 2)
-    # Update YAML file
-    sed -i "s|${PLUGIN_NAME//\//\\/}@.*|${PLUGIN_NAME}@${PLUGIN_VERSION}\"|g; /${PLUGIN_NAME//\//\\/}/{n;n;s|\(^\s*\)\(sha512\).*|\1${PLUGIN_INTEGRITY}|}" "$1"
+    out=$(curl -s -q "https://registry.npmjs.com/${PLUGIN_NAME}/latest" | jq '{id:._id, integrity: .dist.integrity}')
+    package=$(echo "$out" | jq .id)
+    integrity=$(echo "$out" | jq .integrity)
+    yq -i ".backstage.global.dynamic.plugins[] |= select(.package == \"${PLUGIN_NAME}@*\") |= (.package = $package, .integrity = $integrity)"  "$TEMPLATE_FILE"
   done
 }
 
 # Run function to replace plugin versions and integrity in the YAML template
 update_plugins_in_yaml "$TEMPLATE_FILE"
+
